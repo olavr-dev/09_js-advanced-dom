c2d8f52fcf75454d372906f1aa1e020a82713c1b
Olav Rorvik <olavr.dev@gmail.com>
2024-03-21 21:33:20 +0100

    lesson on document selectors

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,7 @@
-document.body.children[2].children[0].href = 'https://google.com';
+// document.body.children[2].children[0].href = 'https://google.com';
+
+let anchorElement = document.getElementById('external-link');
+anchorElement.href = 'https://google.com';
+
+anchorElement = document.querySelector('#external-link');
+anchorElement.href = 'https://olavr.dev';
