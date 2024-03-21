0a4643af457f8f159ded094cd3fd6f79b3d271f7
Olav Rorvik <olavr.dev@gmail.com>
2024-03-21 23:07:34 +0100

    creating, deleting, moving elements

--- scripts/app.js
+++ scripts/app.js
@@ -5,3 +5,34 @@ anchorElement.href = 'https://google.com';
 
 anchorElement = document.querySelector('#external-link');
 anchorElement.href = 'https://olavr.dev';
+
+// ADD AN ELEMENT
+// 1. Create the new element
+
+let newAnchorElement = document.createElement('a');
+newAnchorElement.id = 'js-link';
+newAnchorElement.href = 'https://google.com';
+newAnchorElement.textContent = 'Link to Google.com';
+
+// 2. Get access to the parent that should hold the new element
+
+let firstParagraph = document.querySelector('p');
+
+// 3. Insert the new element into the parent element content
+
+firstParagraph.append(newAnchorElement);
+
+// REMOVE AN ELEMENT
+// 1. Select the element that should be removed
+
+let jsLink = document.querySelector('#js-link');
+
+// 2. Remove the element
+
+jsLink.remove();
+// For older browsers (IE)
+// firstH1Element.parentElement.removeChild(firstH1Element);
+
+// MOVE ELEMENTS
+
+firstParagraph.parentElement.append(firstParagraph);
