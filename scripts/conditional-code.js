66b5d07252ee971d7dbbd1c543526270ca9b40c2
Olav Rorvik <olavr.dev@gmail.com>
2024-03-24 10:20:08 +0100

    truthy or falsy values

--- scripts/conditional-code.js
+++ scripts/conditional-code.js
@@ -3,3 +3,21 @@ const myName = 'Olav';
 if (myName === 'Olav') {
   console.log('Hello, Olav!');
 }
+
+const isLoggedIn = true;
+
+if (!isLoggedIn) {
+  console.log('User is NOT logged in!');
+}
+
+// Truthy and Falsy Values
+
+const enteredUserName = 'Olav';
+
+if (enteredUserName.length > 0) {
+  console.log('Input is valid!');
+}
+
+if (enteredUserName) {
+  console.log('Input is valid!');
+}
