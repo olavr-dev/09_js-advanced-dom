fc8063f902b48d16070ccd48dd1685aafe940747
Olav Rorvik <olavr.dev@gmail.com>
2024-03-24 14:57:30 +0100

    learning for in loops

--- loops-in-action/loops-in-action.js
+++ loops-in-action/loops-in-action.js
@@ -29,10 +29,39 @@ const highlightLinksButtonElement = document.querySelector(
 );
 
 function highlightLinks() {
-  const links = document.querySelectorAll('#highlight-links a');
-  for (link of links) {
-    link.className = 'highlight';
+  const anchorElements = document.querySelectorAll('#highlight-links a');
+  for (const anchorElement of anchorElements) {
+    anchorElement.classList.add('highlight');
   }
 }
 
 highlightLinksButtonElement.addEventListener('click', highlightLinks);
+
+// ---------------------------------------------//
+// ----- Third Exercise: Display User Data -----//
+// ---------------------------------------------//
+
+const userData = {
+  firstName: 'Olav',
+  lastName: 'Øye Rørvik',
+  age: 42,
+  joinDate: '24-04-2024',
+  isAdmin: true,
+};
+const userDataButtonElement = document.querySelector('#user-data button');
+
+function displayUserData() {
+  const outputUserDataElement = document.querySelector('#output-user-data');
+
+  // Clear any existing lists before executing the loop
+  outputUserDataElement.innerHTML = '';
+
+  for (const key in userData) {
+    const newUserDataListElement = document.createElement('li');
+    const outputText = key.toUpperCase() + ': ' + userData[key];
+    newUserDataListElement.textContent = outputText;
+    outputUserDataElement.append(newUserDataListElement);
+  }
+}
+
+userDataButtonElement.addEventListener('click', displayUserData);
